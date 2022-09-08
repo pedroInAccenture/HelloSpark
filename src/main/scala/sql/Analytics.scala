c8aef45db4cf4e8695378032d1ad4d00e3465fd9
pedro.a.ortega <pedro.a.ortega@accenture.com>
2022-09-08 12:41:06 -0500

    se agregan ajercicios de lectura y escritura de archivos

--- src/main/scala/sql/Analytics.scala
+++ src/main/scala/sql/Analytics.scala
@@ -7,11 +7,79 @@ import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions.{col, lit}
 import utils.{Constant, LoadConf}
 
-object Analytics extends App {
+object analytics {
+
+  def main(args: Array[String]): Unit = {
+//    executeCsv
+//    executeAvroToParquet
+      executeParquetToParquet
+  }
+
+}
+
+object executeCsv{
+  /**
+   * Spark settings
+   */
+  val logger = Logger.getLogger (this.getClass.getName)
+
+  val spark: SparkSession = SparkSession.builder ()
+  .master ("local[1]")
+  .appName ("example")
+  .getOrCreate ()
+
 
   /**
-    *  Spark settings
-    */
+   * PARAMETERS
+   */
+  val conf: Config = LoadConf.getConfig
+
+
+  /**
+   * INPUTS
+   */
+  logger.info ("=====> Reading file")
+
+  val df = spark.read
+  .option ("header", true)
+  .csv (conf.getString ("input.path") )
+
+
+  /**
+   * TRANSFORMATIONS
+   */
+  logger.info ("=====> Transform data")
+  val dfTransformed = df.select (col ("*"), lit (1).alias ("literal") )
+
+
+  /**
+   * OUTPUT
+   */
+  logger.info ("=====> Writing file")
+  //  dfTransformed.write.mode("overwrite")
+  //    .csv(conf.getString("output.path"))
+
+//  dfTransformed.write.format("avro")
+//    .mode("overwrite")
+//    .partitionBy("tr")
+//    .save(conf.getString ("output.pathAvro"))
+
+  //  dfTransformed.write.mode("overwrite").parquet(conf.getString("output.pathParquet"))
+
+  logger.info ("=====> sleeping")
+  logger.warn ("=====> sleeping")
+  logger.error ("=====> sleeping")
+
+  logger.info ("=====> reading avro partition")
+
+
+//  Thread.sleep (1000000)
+}
+
+object executeAvroToParquet{
+  /**
+   * Spark settings
+   */
   val logger = Logger.getLogger(this.getClass.getName)
 
   val spark: SparkSession = SparkSession.builder()
@@ -21,43 +89,68 @@ object Analytics extends App {
 
 
   /**
-    *  PARAMETERS
+   * PARAMETERS
    */
-  val conf:Config = LoadConf.getConfig
+  val conf: Config = LoadConf.getConfig
 
 
   /**
-    * INPUTS
+   * INPUTS
    */
-  logger.info("=====> Reading file")
 
-  val df = spark.read
-    .option("header",true)
-    .csv(conf.getString("input.path"))
+  val dfAvro = spark.read
+    .format("avro")
+    .load(conf.getString ("output.pathAvro"))
+    .where(col("tr") === "01")
 
+  dfAvro.show()
 
-  /**
-   * TRANSFORMATIONS
-   */
-  logger.info("=====> Transform data")
-  val dfTransformed = df.select(col("*"),lit(1).alias("literal"))
-
-
-  /**
-   * OUTPUT
-   */
-  logger.info("=====> Writing file")
-//  dfTransformed.write.mode("overwrite")
-//    .csv(conf.getString("output.path"))
-
-  dfTransformed.write.format("avro").save(conf.getString("output.pathAvro"))
-
-//  dfTransformed.write.mode("overwrite").parquet(conf.getString("output.pathParquet"))
-
-  logger.info("=====> sleeping")
-  logger.warn("=====> sleeping")
-  logger.error("=====> sleeping")
-  Thread.sleep(1000000)
+  dfAvro.write.partitionBy("age")
+    .parquet(conf.getString ("output.pathParquet"))
 
 }
 
+object executeParquetToParquet{
+  /**
+   * Spark settings
+   */
+  val logger = Logger.getLogger(this.getClass.getName)
+
+  val spark: SparkSession = SparkSession.builder()
+    .master("local[1]")
+    .appName("example")
+    .getOrCreate()
+
+
+  /**
+   * PARAMETERS
+   */
+  val conf: Config = LoadConf.getConfig
+
+
+  /**
+   * INPUTS
+   */
+  logger.info("=====> Reading file")
+
+//  val dfParquet = spark.read.parquet(conf.getString ("output.pathParquet")+"/age=22")
+//  case class Person(id:String, name:String, age:String, tr:String)
+//  import spark.implicits._
+
+  val dfClientes = spark.read
+    .parquet(conf.getString ("output.pathParquet"))
+    .where(col("age") === "22" )
+
+
+  dfClientes.show()
+  /**
+   * TRANSFORMATION
+   */
+  val dfAgeCount = dfClientes.groupBy("age").count()
+  dfAgeCount.show()
+
+  dfAgeCount.write.partitionBy("age")
+    .mode("append")
+    .parquet(conf.getString("output.pathNewParquet"))
+
+}
